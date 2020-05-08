679c9f834070fb9a339224fab77dc4b42d13aeb2
Chase <chaserabenn@gmail.com>
2020-05-08 17:00:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,7 +203,7 @@ def big_shoe_rebounds
 
   game_hash.each do |location, v| #keys: home, away
     game_hash[location][:players].each do |player_stats|
-      if player_stats[:player_name] == biggest_shoe.keys[0]
+      if player_stats[:player_name] == name
         return player_stats[:rebounds]
       end
     end
